43b44c98ed1ca1dd237666c811ec34951cf1a832
Stan <zyyu@umich.edu>
2025-12-20 05:32:24 -0500

    update: functions to add and remove breakpoints

--- pdb-helpers/pdb-breakpoints-marker.el
+++ pdb-helpers/pdb-breakpoints-marker.el
@@ -1,13 +1,16 @@
 (define-minor-mode pdb-breakpoints-marker-mode
   "show breakpointss when using pdb"
   :lighter " pdb-bp"
-  :global t
+  :global
+  t
   (if pdb-breakpoints-marker-mode
-	  (pdb-breakpoints-marker-mode-enable)
-	(pdb-breakpoints-marker-mode-disable)))
+      (pdb-breakpoints-marker-mode-enable)
+    (pdb-breakpoints-marker-mode-disable)))
 
 (defun pdb-breakpoints-marker-mode-enable ()
-  (advice-add 'gud-pdb-marker-filter :around #'pdb-breakpoints-marker-main))
+  (advice-add
+   'gud-pdb-marker-filter
+   :around #'pdb-breakpoints-marker-main))
 
 (defun pdb-breakpoints-marker-mode-disable ()
   (advice-remove 'gud-pdb-marker-filter #'pdb-breakpoints-marker-main)
@@ -21,3 +24,21 @@
 (defun pdb-breakpoints-marker-clearall ()
   (setq pdb-breakpoints-marker-breakpoints '())
   (setq pdb-breakpoints-marker-markers '()))
+
+(defun pdb-breakpoints-marker-add-breakpoint (file line)
+  (let ((bp (cons file (string-to-number line))))
+    (unless (member bp pdb-breakpoints-marker-breakpoints)
+      (push bp pdb-breakpoints-marker-breakpoints)
+      (pdb-breakpoints-marker-add-marker
+       file (string-to-number line)))))
+(defun pdb-breakpoints-marker-remove-breakpoint (file line)
+  (let ((bp (cons file (string-to-number line))))
+    (setq pdb-breakpoints-marker-breakpoints
+          (delete bp pdb-breakpoints-marker-breakpoints))
+    (pdb-breakpoints-marker-remove-marker
+     file (string-to-number line))))
+
+(defface pdb-breakpoints-marker-marker-face '((t :foreground "red")))
+
+(defun pdb-breakpoints-marker-add-marker (file line))
+(defun pdb-breakpoints-marker-remove-marker (file line))
