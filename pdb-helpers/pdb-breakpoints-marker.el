b3af1835ed95fb2e4a0c2ef40ca8296eb92cd337
Stan <zyyu@umich.edu>
2025-12-20 05:19:46 -0500

    update: add data structures

--- pdb-helpers/pdb-breakpoints-marker.el
+++ pdb-helpers/pdb-breakpoints-marker.el
@@ -11,4 +11,13 @@
 
 (defun pdb-breakpoints-marker-mode-disable ()
   (advice-remove 'gud-pdb-marker-filter #'pdb-breakpoints-marker-main)
-  (pdb-breakpoints-marker-clear))
+  (pdb-breakpoints-marker-clearall))
+
+;; store file and position of breakpoint
+(defvar pdb-breakpoints-marker-breakpoints '())
+;; store ui markers
+(defvar pdb-breakpoints-marker-markers '())
+
+(defun pdb-breakpoints-marker-clearall ()
+  (setq pdb-breakpoints-marker-breakpoints '())
+  (setq pdb-breakpoints-marker-markers '()))
