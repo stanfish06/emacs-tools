5617477ff9e82596227a44ae2353a8ccaea8f832
Stan <zyyu@umich.edu>
2025-12-23 00:53:23 -0500

    add: window resize function

--- init.el
+++ init.el
@@ -1,2 +1,4 @@
 (add-to-list 'load-path "~/.emacs.d/emacs-tools/pdb-helpers/")
+(add-to-list 'load-path "~/.emacs.d/emacs-tools/window/")
 (require 'pdb-breakpoints-marker)
+(require 'custom-window-resize)
