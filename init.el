4d82039516982ace263eaa89cf346dca680a357d
Stan <zyyu@umich.edu>
2026-01-15 15:54:31 -0500

    update: org agenda

--- init.el
+++ init.el
@@ -1,4 +1,6 @@
 (add-to-list 'load-path "~/.emacs.d/emacs-tools/pdb-helpers/")
 (add-to-list 'load-path "~/.emacs.d/emacs-tools/window/")
+(add-to-list 'load-path "~/.emacs.d/emacs-tools/org/")
 (require 'pdb-breakpoints-marker)
 (require 'custom-window-resize)
+(require 'agenda)
---
+++ org/agenda.el
@@ -0,0 +1,20 @@
+(require 'org)
+
+(if (not (file-exists-p "~/org-agenda.org"))
+    (with-temp-file "~/org-agenda.org"
+      (insert "#+TITLE: Agenda\n\n* Tasks\n")))
+(setq org-agenda-files '("~/org-agenda.org"))
+(global-set-key
+ (kbd "C-c A")
+ (lambda ()
+   (interactive)
+   (find-file "~/org-agenda.org")))
+;; todo template
+(setq org-capture-templates
+      '(("t"
+         "Todo"
+         entry
+         (file+headline "~/org-agenda.org" "Tasks")
+         "** TODO %?\n  SCHEDULED: %t\n")))
+;; TODO: add more template and helper functions
+(provide 'agenda)
